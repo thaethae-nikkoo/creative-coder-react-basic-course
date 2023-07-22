12975f677ba037dee7dedaefe5d6b5932c4e6fa1
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-07-22 19:08:07 +0630

    Episode 13 Final Commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,25 @@ import { useState } from "react";
 function App() {
   // let name = "thae nandar soe";
   let [name, setName] = useState("thae nandar soe");
+  let [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "First Post",
+    },
+    {
+      id: 2,
+      title: "Second Post",
+    },
+    {
+      id: 3,
+      title: "Third Post",
+    },
+    {
+      id: 4,
+      title: "Fourth Post",
+    },
+  ]);
+  console.log(posts);
   let changeName = () => {
     // name = "pyae sone aung";
     setName("pyae sone aung");
@@ -17,6 +36,11 @@ function App() {
         My name is <strong>{name}</strong>
       </p>
       <button onClick={changeName}>Change Name</button>
+      <ul>
+        {posts.map((post) => (
+          <li key={post.id}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
