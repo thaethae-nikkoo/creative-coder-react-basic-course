43a81a32459f0083d7c3035c60b5616bfe635f2d
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-07-22 14:09:36 +0630

    Episode 10 Final commit

--- src/App.js
+++ src/App.js
@@ -3,24 +3,17 @@ import "./App.css";
 import Image from "./assets/moonsoon.jpg";
 
 function App() {
+  let name = "thae nandar soe";
+  let changeName = () => {
+    name = "pyae sone aung";
+    console.log(name);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React Carefully
-        </a>
-        {/* <img src="/moonsoon.jpg" width="300" height="300" alt="" /> */}
-        <img src={Image} width="300" height="300" alt="" />
-      </header>
+      <p className="paragraph">
+        My name is <strong>{name}</strong>
+      </p>
+      <button onClick={changeName}>Change Name</button>
     </div>
   );
 }
