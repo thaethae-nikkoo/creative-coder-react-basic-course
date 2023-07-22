e8ba0498fb79f65008c479e9e05ebdf287af38f4
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-07-22 19:23:13 +0630

    Episode 14 Final commit

--- src/App.js
+++ src/App.js
@@ -1,48 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-import Image from "./assets/moonsoon.jpg";
-import { useState } from "react";
+import React, { useState } from "react";
 
-function App() {
-  // let name = "thae nandar soe";
-  let [name, setName] = useState("thae nandar soe");
-  let [posts, setPosts] = useState([
-    {
-      id: 1,
-      title: "First Post",
-    },
-    {
-      id: 2,
-      title: "Second Post",
-    },
-    {
-      id: 3,
-      title: "Third Post",
-    },
-    {
-      id: 4,
-      title: "Fourth Post",
-    },
-  ]);
-  console.log(posts);
-  let changeName = () => {
-    // name = "pyae sone aung";
-    setName("pyae sone aung");
-    console.log(name);
+export default function App() {
+  let [count, setCount] = useState(0);
+  let increment = () => {
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
+    setCount((prevState) => prevState + 1);
   };
   return (
-    <div className="App">
-      <p className="paragraph">
-        My name is <strong>{name}</strong>
-      </p>
-      <button onClick={changeName}>Change Name</button>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>{post.title}</li>
-        ))}
-      </ul>
+    <div>
+      <h1>Counter</h1>
+      <h3>Count - {count}</h3>
+      <button onClick={increment}>Increment</button>
     </div>
   );
 }
-
-export default App;
