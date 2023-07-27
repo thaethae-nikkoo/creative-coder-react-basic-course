1bf2bc32799d1684bc69793018883563555acdbe
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-07-27 22:36:14 +0630

    Episode 27 Final commit

--- src/components/PostForm/index.js
+++ src/components/PostForm/index.js
@@ -5,17 +5,29 @@ export default function PostForm() {
   let onChangeHandler = (e) => {
     setTitle(e.target.value);
   };
+  let resetForm = () => {
+    setTitle("");
+    console.log("updated successfully");
+  };
   return (
     <form className="post-form">
       <h1>Create Post</h1>
       <div className="form-control">
         <label htmlFor="">Title</label>
-        <input type="text" placeholder="Title" onChange={onChangeHandler} />
+        <input
+          type="text"
+          placeholder="Title"
+          onChange={onChangeHandler}
+          value={title}
+        />
 
         {/* အပေါ်က onChangeHandler function ကို ဖြုတ်ပီး ဒီလိုရေးလို့ရတယ် */}
         {/*  <input type="text" placeholder="Title" onChange={(e)=>setTitle(e.target.value)} /> */}
         <p>{title}</p>
       </div>
+      <button type="button" onClick={resetForm}>
+        Reset Form
+      </button>
 
       <div className="form-control">
         <button>Post Now</button>
