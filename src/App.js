bd8f2c83daf3cfa38808642df5b694b71eb17b0e
thaethae-nikkoo <thaenandarsoe.2201@gmail.com>
2023-08-06 22:30:09 +0630

    Episode 37 final commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import "./App.css";
 import TripList from "./components/TripList/index";
 
 function App() {
   let [show, setShow] = useState(true);
+  let [data, setData] = useState("mydata");
+
+  let myFun = () => {
+    setData("update data");
+  };
+  useEffect(() => {
+    myFun();
+    console.log("running");
+  }, [myFun]);
 
   return (
     <>
+      <h1>{data}</h1>
       <button onClick={() => setShow(false)}>Hide</button>
       {show && <TripList />}
     </>
